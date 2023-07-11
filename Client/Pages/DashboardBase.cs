c83b09a4dde34e208a4f38008e93b7ce03c16d63
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2023-07-11 09:53:47 +0200

    index page update, mainlayout update andded dashboard pages

---
+++ Client/Pages/DashboardBase.cs
@@ -0,0 +1,40 @@
+﻿using BlazorApp1.Client.Services.Interfaces;
+using BlazorApp1.Shared.AppUserDTO;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorApp1.Client.Pages
+{
+    public class DashboardBase : ComponentBase
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        public ILocalStorageService localStorage { get; set; }
+
+        [Inject]
+        public IAccountService AccountService { get; set; }
+
+        public string Company;
+        public string UserName { get; set; }
+        public AppUserDTO AppUser { get; set; } = new AppUserDTO();
+        public IEnumerable<AppUserDTO> user { get; set; } = new List<AppUserDTO>();
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+        public async Task SetCompanyFromLocalStorage()
+        {
+            //await JS.InvokeVoidAsync("localstorage.setItem", "Company", Company);
+            await localStorage.SetItemAsync("Company", Company);
+        }
+
+        public async Task GetCompanyFromLocalStorage()
+        {
+            //var localStorageCompany = await JS.InvokeAsync<string>("localstorage.getItem","Company");
+            var localStorageCompany = await localStorage.GetItemAsync<string>("Company");
+        }
+    }
+}
