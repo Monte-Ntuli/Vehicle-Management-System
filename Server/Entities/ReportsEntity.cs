0377b76ed0b4ab7bcc6e3e381fa7318079d72071
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2023-10-31 18:14:52 +0200

    Report Testing

---
+++ Server/Entities/ReportsEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Server.Entities
+{
+    public class ReportsEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public int ReportID { get; set; }
+        public int EmployeeID { get; set; }
+        public int VehicleID { get; set; }
+        public string Company { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
